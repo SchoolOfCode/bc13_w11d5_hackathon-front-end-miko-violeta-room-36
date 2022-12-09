9a2cab0e82fa119a6e67fc6f67aa6f3ff0713a3e
violetacf <violeta@cique.es>
2022-12-09 12:06:34 +0000

    dark-mode working

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,17 +1,18 @@
-import React, { useEffect, useState } from "react";
-import Popup from "../Popup/Popup";
-// import {DarkModeContext} from '../DarkModeContext.js'
+import React, { useEffect, useState, useContext } from 'react';
+import Popup from '../Popup/Popup';
+import Button from '../Button/Button';
+import { DarkModeContext } from '../DarkModeContext.js';
 
-import "./App.css";
+import './App.css';
 
 function App() {
   const [daySubArr, setDaySubArr] = useState([]);
-  //   const {darkMode}  = useContext(DarkModeContext)
-  //   const {toggleDarkMode} = useContext(DarkModeContext)
+  const { darkMode } = useContext(DarkModeContext);
+  const { toggleDarkMode } = useContext(DarkModeContext);
 
   useEffect(() => {
     async function getYoga() {
-      const response = await fetch("http://localhost:3001/api");
+      const response = await fetch('http://localhost:3001/api');
       const data = await response.json();
       console.log(data);
       setDaySubArr(data.payload);
@@ -19,7 +20,7 @@ function App() {
     getYoga();
   }, []);
   return (
-    <div className="app-container">
+    <div className={darkMode ? `dark` : `app-container`}>
       <div className="header-container">
         <h1>Advent of Yoga!</h1>
       </div>
@@ -27,16 +28,26 @@ function App() {
         {daySubArr.map((day) => {
           return (
             <div className="day-container">
-            <Popup
-              dayButton={day.id}
-              dayHeader={day.id}
-              dayName={day.name}
-              dayImg={day.img_link}
-              dayDescriptionPopup={day.description}
-            />
+              <Popup
+                dayButton={day.id}
+                dayHeader={day.id}
+                dayName={day.name}
+                dayImg={day.img_link}
+                dayDescriptionPopup={day.description}
+              />
             </div>
           );
         })}
+      </div>
+      <div className="button-container">
+        <h2>
+          Click the button to do Yoga {darkMode ? `during the day` : `at night`}
+          !
+        </h2>
+        <Button
+          buttonText={darkMode ? `Light Mode` : `Dark Mode`}
+          buttonClick={toggleDarkMode}
+        />
       </div>
     </div>
 
